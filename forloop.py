149a5876810e52e7a310b68d466fcae6d42e4648
unknown <sagarzopeoffical2002@gmail.com>
2022-09-16 10:45:49 -0700

    python class file is added

---
+++ forloop.py
@@ -0,0 +1,14 @@
+# list print zali 
+
+# name = ["sagar","vivek","pintu","girish"]
+# print(name)
+
+# for loop ne value return kelya
+
+# for newname in name:
+#     print(newname)
+
+# rang find karnyasathi
+
+for i in range(1,101):
+    print(i)
